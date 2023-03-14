1c9c157887e01c656d89f3a9799dfb9989ad7ee9
Ella Byun <yantle@naver.com>
2023-03-14 13:14:29 +0900

    use multer middleware upload file

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const { Db } = require('mongodb');
+const multer = require('multer');
+
+const storageConfig = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'images');
+  },
+  filename: function(req, file, cb){
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
@@ -9,5 +22,16 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+router.post('/profiles', upload.single('image'), function(req,res){
+  //multer 사용할 라우터, 특정 라우터에만 적용되는 미들웨어 사용 예!
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect('/');
+})
 
 module.exports = router;
